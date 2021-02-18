22feaaa9d83e8fb9e24104e0f4cd3e48830f8e6a
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-02-18 13:05:11 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+try:
+    from tkinter import *
+except ImportError:
+    from Tkinter import *
+import time
+from pwgenfunc import RandPass
+#=====================================METHODS===================================
+def pwGenerator(size = 8):
+    data = RandPass(size)
+    new_password = data[0]
+    pw_strength = data[1]
+    pw_color = data[2]
+    PASSWORD.set(new_password);
+    label_strength.configure(foreground="white", background=pw_color, text=pw_strength, font=('Segoe UI', 10, 'bold'), bd=10, height=1, width=10)
+    gui.clipboard_clear()
+    gui.clipboard_append(new_password)
+    gui.update()
+    time.sleep(.02)
+    gui.update()
+    gui.mainloop()
+
+#=====================================WINDOW===================================
+gui = Tk()
+gui.title("Password Generator")
+gui.config(bg = '#1A1A1A')
+gui.title('Resizable') 
+width = 600
+height = 342
+screen_width = gui.winfo_screenwidth()
+screen_height = gui.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+gui.geometry("%dx%d+%d+%d" % (width, height, x, y))
+
+
+#====================================VARIABLES==================================
+PASSWORD = StringVar()
+PW_SIZE = IntVar()
+e1 = Entry(gui, text=PW_SIZE)
+PW_SIZE.set(8) # sets the default value for PW size/length
+
+#====================================FRAME======================================
+Top = Frame(gui, width=width)
+
+Top.pack(side=TOP)
+Top.config(bg = '#1A1A1A')
+Form = Frame(gui, width=width, background="#1A1A1A",)
+Form.pack(side=TOP)
+Bot = Frame(gui, width=width)
+Bot.pack(side=BOTTOM)
+#====================================LABEL WIDGET===============================
+#label_title = Label(Top, width=width, font=('Segoe UI', 12, 'bold'), background="#1A1A1A", text="Select: Size >> Click: Generate Now", bd=1, relief=SOLID)
+#label_title.pack(fill=X)
+label_password = Label(Form, font=('Segoe UI', 18), text="Password",foreground="white", background="#1A1A1A", bd=10)
+label_password.grid(row=0, pady=10)
+label_strength = Label(Form, font=('Segoe UI', 10, 'bold'), foreground="white", background="white", text="Weak", bd=10, height=1, width=10)
+label_strength.grid(row=0, column=3, pady=10, padx=10)
+label_pw_size = Label(Form, font=('Segoe UI', 18), text="Size",foreground="white", background="#1A1A1A", bd=10)
+label_pw_size.grid(row=2, pady=10)
+label_instructions = Label(Bot, width=width, font=('Segoe UI', 12, 'bold'), text="Password Generated to your Clipboard!", foreground="white", background="#1A1A1A", bd=1, relief=SOLID)
+label_instructions.pack(fill=X)
+#====================================ENTRY WIDGET===============================
+password = Entry(Form, textvariable=PASSWORD, font=(18), width=24)
+password.grid(row=0, column=1, columnspan=2)
+pw_size = Scale(Form, from_=8, to=24, length=200,width=24,sliderlength=14, orient=HORIZONTAL, variable=PW_SIZE, foreground="white", background="#1A1A1A",  font=(16))
+pw_size.grid(row=2, column=1, columnspan=2)
+
+def Copy_password():
+    pyperclip.copy(new_password_str.get())
+
+Button(Top, text = 'COPY TO CLIPBOARD', foreground="white", background="#1A1A1A", command = Copy_password).pack(pady=5)
+
+#====================================BUTTON WIDGET==============================
+btn_generate = Button(Form, text="Generate Now", width=20, command=lambda: pwGenerator(PW_SIZE))
+btn_generate.grid(row=4, column=1, columnspan=2)
+
+#=======================================INITIATOR=================================
+gui.resizable(False, False)
+gui.mainloop()
---
+++ pwgenfunc.py
@@ -0,0 +1,76 @@
+try:
+    from string import ascii_letters, digits, punctuation, join
+except ImportError:
+    from string import ascii_letters, digits, punctuation
+from random import choice, sample, randint
+# specific imports to make this small, fast, efficient
+# to generate a password with specific restrictions: \
+# NO numbers or special characters in 1st position
+# NO space at the end of the string
+
+#MainFunc
+def isEven(integer):
+    """Return Boolean: True if input is even, False if not."""
+    return integer % 2 == 0
+
+def RandPass(size = 8):
+    """This is the password generator"""
+    s0 = "!@#$%^&*- _~+-=" # this set of special characters contains a space 
+    s1 = ascii_letters # upper AND lower cases
+    s3 = digits
+    # 's0' can be CUSTOMIZED to include only allowed characters
+    
+    s = s0 + s1
+    s_full = s + s3
+    passlen = size.get()
+    new_password = ""
+
+    # assigning specific sizes for each section of the pw generated
+    if isEven(passlen) == True:
+        front = passlen // 5
+    else:
+        front = passlen // 2
+    mid = 2
+    previous = passlen - (front + mid) - 1
+
+    pass0 = "".join(choice(s0)) # forces a minimum number of punctuations in the middle
+    pass1 = "".join(sample(s_full,front ))
+    pass2 = "".join(sample(s3,mid))
+    # NO punctuations as 1st character!!! 
+    pass3 = "".join(sample(s, previous ))
+    # sometimes integer division reduces the size of the desired password length, the following adjusts it back
+    if passlen != len(pass0 + pass1 + pass2 + pass3):
+        pass2 = "".join(sample(s3,passlen - (front+previous+1) ))
+
+    if pass3[:-1] == ' ': # to avoid having an empty space at the end of password
+        temp = list(pass3)
+        temp[:-1] = choice(s)
+        pass3 = ''.join(str(e) for e in temp)
+    new_password = pass0 + pass1 + pass2 + pass3    
+    
+    if passlen <= 8:
+        msg = 'VERY WEAK'
+        colorVal = "#6d0001"
+    elif passlen <=10:
+        msg = 'WEAK'
+        colorVal = "#cc0000"
+    elif passlen <=12:
+        msg = 'DECENT'
+        colorVal = "#fc8600"
+    elif passlen <=14:
+        msg = 'GOOD'
+        colorVal = "#eae200"
+    elif passlen <=16:
+        msg = 'STRONG'
+        colorVal = "#9ff400"
+    elif passlen <=18:
+        msg = 'VERY STRONG'
+        colorVal = "#007715"
+    elif passlen >18:
+        msg = 'EXCELLENT'
+        colorVal = "#001fef"
+    else:
+        pass
+
+    return new_password, msg, colorVal
+
